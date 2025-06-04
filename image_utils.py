e7136e18437da96e1a404919eef38395d185834f
fabiofidone1978 <77326821+fabiofidone1978@users.noreply.github.com>
2025-06-04 10:44:15 +0200

    Use env var for API key and validate request

--- image_utils.py
+++ image_utils.py
@@ -1,16 +1,26 @@
+import os
 from PIL import Image, ImageFilter
 
 
+ASSETS_DIR = "assets"
+
+
+def ensure_assets_dir():
+    os.makedirs(ASSETS_DIR, exist_ok=True)
+
+
 def apply_filter(path, filter_type="BLUR"):
+    ensure_assets_dir()
     img = Image.open(path)
     if filter_type == "BLUR":
         img = img.filter(ImageFilter.BLUR)
     elif filter_type == "CONTOUR":
         img = img.filter(ImageFilter.CONTOUR)
-    img.save("assets/output.jpg")
+    img.save(os.path.join(ASSETS_DIR, "output.jpg"))
 
 
 def resize_image(path, size=(512, 512)):
+    ensure_assets_dir()
     img = Image.open(path)
     img = img.resize(size)
-    img.save("assets/resized.jpg")
+    img.save(os.path.join(ASSETS_DIR, "resized.jpg"))
